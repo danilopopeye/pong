8f28e9bf6038a2be67b8bc664d2868eb131dc38f
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-05-27 09:13:20 -0300

    Basic layout;

--- pong.js
+++ pong.js
@@ -1,6 +1,26 @@
 (function(){
 	var p = window.Pong = {
-		init: function(){ }
+		width: window.innerWidth,
+		width: window.innerWidth,
+		timer: null,
+		init: function(){
+			$(document).keypress( p.keypress );
+			
+			p.timer = setInterval( p.gameplay, 100 );
+		},
+		keypress: function(e){
+			switch( e.keyCode ){
+				case 38:
+					console.log( 'UP' ); break;
+				case 40:
+					console.log( 'DOWN' ); break;
+				default:
+					console.log( e.keyCode );
+			}
+		},
+		gameplay: function(){
+			
+		}
 	};
 
 	$( p.init );
