a8867c98a6d310f57eca292d4214c5a91373613c
danilopopeye <danilopopeye@gmail.com>
2010-05-27 18:14:32 -0300

    Play / Pause and basic paddle moviment;

--- pong.js
+++ pong.js
@@ -13,24 +13,36 @@
 		speed: 5,
 		// cache of ball element
 		ball: null,
+		// cache of your paddle
+		you: null,
 
 		init: function(){
-			$(document).bind(
-				$.browser.msie ? 'keydown' : 'keypress', p.keypress
-			);
+			$(document).bind('keydown', p.keypress);
 
 			p.ball = $('#ball');
+			p.you = $('#you');
 
+			p.start();
+		},
+		start: function(){
 			p.timer = setInterval( $.proxy( p.gameplay, p ), 10 );
 		},
 		stop: function(){
 			clearInterval( this.timer );
+			this.timer = null;
 		},
 		keypress: function(e){
+		console.log( +new Date() );
 			switch( e.keyCode ){
+				case 00:
+				case 13:
+				case 27:
+					p.timer === null ? p.start() : p.stop(); break;
 				case 38:
+					p.User.up();
 					console.log( 'UP' ); break;
 				case 40:
+					p.User.down();
 					console.log( 'DOWN' ); break;
 				default:
 					console.log( e.keyCode );
@@ -76,6 +88,19 @@
 		},
 		toggleY: function(){
 			this.direction.y = !this.direction.y;
+		},
+		//TODO: abstract this to a user class
+		User: {
+			up: function(){
+				p.you.css({
+					top: ( p.you.position().top - p.speed * 2 ) + 'px'
+				});
+			},
+			down: function(){
+				p.you.css({
+					top: ( p.you.position().top + p.speed * 2 ) + 'px'
+				});
+			}
 		}
 	};
 
