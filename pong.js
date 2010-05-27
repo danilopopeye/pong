27667440684f9337733155ee201a5a7fc6466974
Danilo Sousa Gonçalves <dsgoncalves@sp.r7.com>
2010-05-27 11:46:26 -0300

    The ball moves \o/

--- pong.js
+++ pong.js
@@ -1,12 +1,30 @@
 (function(){
 	var p = window.Pong = {
+		// max size of stage
 		width: window.innerWidth,
-		width: window.innerWidth,
+		height: window.innerHeight,
+		// timer id
 		timer: null,
+		// directions of the ball
+		direction: {
+			x: true, y: false
+		},
+		// speed of the ball
+		speed: 5,
+		// cache of ball element
+		ball: null,
+
 		init: function(){
-			$(document).keypress( p.keypress );
-			
-			p.timer = setInterval( p.gameplay, 100 );
+			$(document).bind(
+				$.browser.msie ? 'keydown' : 'keypress', p.keypress
+			);
+
+			p.ball = $('#ball');
+
+			p.timer = setInterval( $.proxy( p.gameplay, p ), 10 );
+		},
+		stop: function(){
+			clearInterval( this.timer );
 		},
 		keypress: function(e){
 			switch( e.keyCode ){
@@ -19,7 +37,45 @@
 			}
 		},
 		gameplay: function(){
-			
+			var pos = p.ball.position();
+
+			p.checkColisions( pos )
+
+			p.moveBall( pos );
+		},
+		checkColisions: function(pos){
+			if( pos.top <= 0 || ( pos.top + 10 ) >= this.height ){
+				this.toggleY();
+			}
+
+			if( pos.left <= 0 || ( pos.left + 10 ) >= this.width ){
+				this.toggleX();
+			}
+		},
+		moveBall: function(pos){
+			this._move.call(
+				this.ball,
+				this._calcMove( 'x', pos.left ),
+				this._calcMove( 'y', pos.top )
+			);
+		},
+		_move: function(x,y){
+			this.css({
+				left: x, top: y
+			});
+		},
+		_calcMove: function(z,value){
+			return (
+				this.direction[ z ]
+					? ( value + this.speed )
+					: ( value - this.speed )
+			) + 'px';
+		},
+		toggleX: function(){
+			this.direction.x = !this.direction.x;
+		},
+		toggleY: function(){
+			this.direction.y = !this.direction.y;
 		}
 	};
 
